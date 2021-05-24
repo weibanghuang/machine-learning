9a62d2e11e2983b6b5b731a13ecc992c90c19fb4
weibanghuang <weibanghuang@users.noreply.github.com>
2021-05-24 01:37:15 -0400

    week5

--- machine-learning/machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning/machine-learning-ex4/ex4/nnCostFunction.m
@@ -21,8 +21,6 @@ Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
 
 Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
                  num_labels, (hidden_layer_size + 1));
-size(Theta1)
-size(Theta2)
 % Setup some useful variables
 m = size(X, 1);
          
@@ -30,7 +28,6 @@ m = size(X, 1);
 J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: You should complete the code by working through the
 %               following parts.
@@ -62,31 +59,59 @@ Theta2_grad = zeros(size(Theta2));
 %               the regularization separately and then add them to Theta1_grad
 %               and Theta2_grad from Part 2.
 %
+X=[ones(m,1), X];
+yi=zeros(size(y, 1), num_labels); %5000x10
+for t = 1:m
+    yi(t, y(t))=1;
+end
 
+z2 = X*transpose(Theta1); %5000x25
+a2 = sigmoid(z2); %5000x25
+a2 = [ones(size(a2,1),1),a2]; %5000x26
+z3 = a2*transpose(Theta2); %5000x10
+h0x = sigmoid(z3); %5000x10
 
+a=-yi.*log(h0x); %5000x10
+b=(1-yi).*log(1-h0x); %5000x10
+J = (1/m)*sum(sum(a-b));
+Theta1_copy=Theta1;
+Theta2_copy=Theta2;
+Theta1_copy(:,1) = []; %remove first column 25x401->25x400
+Theta2_copy(:,1) = []; %remove first column 10x26->10x25
 
+reg = (lambda/(2*m))*(sum(sum(Theta1_copy.^2))+sum(sum(Theta2_copy.^2)));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+J += reg;
 % -------------------------------------------------------------
 
+pussy = zeros(m,size(Theta2,1)); %5000x10
+
+for t = 1:m
+
+a1 = X(t,:); %1x401
+z2 = a1*transpose(Theta1); %1x25
+a2 = sigmoid(z2); %1x25
+a2 = [1, a2]; %1x26
+z3 = a2*transpose(Theta2); %1x10
+a3 = sigmoid(z3); %1x10
+pussy(t,:)=a3; %this is literally the same thing as the code above. I just forgot this line when I was writing the code above, and I was wonderin why my code wasnt working. smfh.
+delta3 = a3 - yi(t, :);%1x10
+delta2 = (delta3*Theta2_copy).*sigmoidGradient(z2); %1x25
+
+Theta2_grad += transpose(delta3)*a2;
+Theta1_grad += transpose(delta2)*a1;
+
+end
+
+Theta2_grad /= m;
+Theta1_grad /= m;
 % =========================================================================
+
+%regularization
+Theta1_grad(:,2:size(Theta1_grad,2)) += (lambda/m) * Theta1(:,2:size(Theta1,2));
+Theta2_grad(:,2:size(Theta2_grad,2)) += (lambda/m) * Theta2(:,2:size(Theta2,2));
 
 % Unroll gradients
 grad = [Theta1_grad(:) ; Theta2_grad(:)];
-
 
 end
--- machine-learning/machine-learning-ex4/ex4/sigmoidGradient.m
+++ machine-learning/machine-learning-ex4/ex4/sigmoidGradient.m
@@ -12,15 +12,19 @@ g = zeros(size(z));
 % Instructions: Compute the gradient of the sigmoid function evaluated at
 %               each value of z (z can be a matrix, vector or scalar).
 
+
+
+%{
+//I wrote this code before reading the pdf. lol
+//it's the same thing as g(1-g)
 skin = 1e-4;
 peniswithskin = sigmoid(z+skin); 
 %suck my zick plus skin LOL please let me have my laugh T_T 
 peniswithnoskin = sigmoid(z-skin);
 g = (peniswithskin-peniswithnoskin)/(2*skin);
+%}
 
-
-
-
+g=sigmoid(z).*(1-sigmoid(z));
 
 
 
